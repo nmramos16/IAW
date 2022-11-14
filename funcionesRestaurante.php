eb9e516b3a9bb1a3c68c2faef10e7f7ef7047a43
nmramos16 <nmramos16@ieszaidinvergeles.org>
2022-11-14 08:55:56 +0100

    Ejercicio Restaurante

---
+++ funcionesRestaurante.php
@@ -0,0 +1,26 @@
+<?php
+
+//Creamos los arrays con sus claves y valores
+$primeros = array("Ensalada" => 7, "Tabla de Queso" => 8, "Plato de Jamón Ibérico" => 5, "Flamenquines" => 6, "Aceitunas con Pimientos" => 4);
+
+//Los podemos crear de dos maneras, esta es la segunda
+$segundos = ["Macarrones con tomate y carne" => 9, "Filete de pollo empanado" => 10, "Ternera de Asturias" => 13, "Lomo de Canguro" => 12, "Hamburguesa de Buey" => 11];
+
+
+$postre = ["Chocobollo" => 2, "Flan de Huevo" => 3, "Natillas" => 4.5, "Palmeritas" => 3.5, "Café" => 1.5];
+
+// <editor-fold defaultstate="collapsed" desc="MediaPrecios">
+// 
+function mediaPrecios($primeros) {
+    array_sum($primeros)/5;
+    echo mediaPrecios;
+}
+$mediaPrimeros = array_sum($primeros)/5;
+//echo $media;
+$mediaPrimeros = array_sum($primeros)/5;
+        
+$mediaPrimeros = array_sum($primeros)/5;
+
+echo mediaPrecios;
+echo mediaPrecios;
+// </editor-fold>
